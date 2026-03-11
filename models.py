e0b0eb7a33f4821b127ce43dacc77051e9bd60f0
LXGeffort <118441360+LXGeffort@users.noreply.github.com>
2026-03-11 12:58:23 +0800

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # import pymysql
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database/riceDiseasesProject.sqlite3'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database/wheatDiseasesProject.sqlite3'
 # app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:root@localhost:3306/liudao'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "random string"
